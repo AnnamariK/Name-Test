1e4830b861ec4ea5cb8749f019d3a0fd839b0431
Enyem <annamari.keskeny@gmail.com>
2016-05-31 14:55:03 +0100

    Logout page changes

--- ArtTest/src/test/java/templateTestPackage/RunCukes.java
+++ ArtTest/src/test/java/templateTestPackage/RunCukes.java
@@ -1,10 +1,13 @@
+package templateTestPackage;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"	},features="src/test/resources/")
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"	},features="src/test/resources/",
+tags="@bitch")
+
 
 public class RunCukes {
 //Added from Github
