4005883313b784de2d1379e03d7d82c8f9c2ea23
Annamaria <Annamaria@10.3.11.211>
2016-10-26 13:00:33 +0100

    Hello

---
+++ src/test/java/testPackage/HelloWorld.java
@@ -0,0 +1,9 @@
+package testPackage;
+
+public class HelloWorld {
+		
+		public static void main(String[] args){
+			System.out.println("Hello World");
+		}
+}
+//AtTest in NoTemplate project is doing the same but using Junit instead of the Java method, so it doesn't need the MAIN method
